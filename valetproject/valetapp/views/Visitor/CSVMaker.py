a72a566f992e24591f67b092ed3f314c54d209f4
Oisin McNamara🐷🦄🛡️📈 <oisin.mcnamara@gmail.com>
2022-03-07 15:50:59 +0000

    adapter changes made

--- valetproject/valetapp/views/Visitor/CSVMaker.py
+++ valetproject/valetapp/views/Visitor/CSVMaker.py
@@ -1,9 +1,39 @@
 from valetapp.views.Visitor.exportToCSV import exportToCSV_Adapter
-
+import csv
 
 class CSVMaker:
     def __init__(self) -> None:
         pass
 
     def get_emails(self):
-        print(exportToCSV_Adapter().get_customer_emails())
+        return exportToCSV_Adapter().get_customer_emails()
+
+    def get_money(self):
+        return exportToCSV_Adapter.get_total_money
+
+    def get_money_in_store(self):
+        return exportToCSV_Adapter.get_money_by_each_store
+
+    def get_all_stores(self):
+        return exportToCSV_Adapter.get_stores
+
+    def get_all_valets(self):
+        return exportToCSV_Adapter.get_valets
+
+    def get_all_staff(self):
+        return exportToCSV_Adapter.get_staff
+
+    def get_all_valets(self):
+        return exportToCSV_Adapter.get_valets
+
+    def create_CSV(self):
+        header = ['emails', 'money', 'money_in_store', 'all_stores', 'all_valets', 'all_staff', 'all_valets']
+        data = [CSVMaker.get_emails(), CSVMaker.get_money(), CSVMaker.get_money_in_store(), CSVMaker.get_all_stores(), CSVMaker.get_all_valets(), CSVMaker.get_all_staff(), CSVMaker.get_all_valets()]
+    
+        with open('storedata.csv', 'w', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+
+            writer.writerow(header)
+
+            # write the data
+            writer.writerow(data)
